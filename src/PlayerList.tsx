ff39cfd0ed8120623fe9f17336eae36ff361ea09
sadapphone <sandyartha@hotmail.com>
2025-03-19 06:22:06 +0700

    timnas indonesia 1

--- src/PlayerList.tsx
+++ src/PlayerList.tsx
@@ -93,7 +93,7 @@ const IntroTitle: React.FC<{ person?: TopPlayer }> = ({ person }) => {
         </div>
       </div>
 
-      {/* Judul */}
+      {/* Judul 1 */}
       <h1
         style={{
           fontSize: "7rem",
